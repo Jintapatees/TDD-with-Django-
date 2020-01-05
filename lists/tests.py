e967dd420d63f609aeadf1a046dc835166620684
Jintapatees <59291127+Jintapatees@users.noreply.github.com>
2020-01-05 22:07:12 +0700

    Revert "Refactor home page view to use a template"
    This reverts commit 34711f3b64419afd7e3d209883602d9c6c81854b.

--- lists/tests.py
+++ lists/tests.py
@@ -9,18 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')  
         self.assertEqual(found.func, home_page) 
-
     def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')  
-
+        request = HttpRequest()  
+        response = home_page(request)  
         html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'wrong.html')
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>To-Do lists</title>', html)  
+        self.assertTrue(html.endswith('</html>'))
 
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
 
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    return HttpResponse('<html><title>To-Do lists</title></html>')
+
+# Create your views here.
+
